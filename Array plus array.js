c7d7e0b9bb3bf4474d9965f964510547a40be2a6
Vladyslav Synyshen <sini4vlad@gmail.com>
2022-03-12 17:33:20 +0200

    Update Array plus array.js

--- Array plus array.js
+++ Array plus array.js
@@ -1,3 +1,7 @@
 // https://www.codewars.com/kata/5a2be17aee1aaefe2a000151/train/javascript
 
 const arrayPlusArray = (arr1, arr2) => arr1.reduce((x, y) => x + y) + arr2.reduce((x, y) => x + y);
+
+// aktualisiert
+
+const arrayPlusArray = (arr1, arr2) => arr1.concat(arr2).reduce((x, y) => x + y);
